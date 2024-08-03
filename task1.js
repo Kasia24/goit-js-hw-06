36dd209c3f6071cd90cea9ac3505f05a7d607929
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-08-03 11:03:24 +0200

    add commit

--- task1.js
+++ task1.js
@@ -1,27 +1,6 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return balance;
-  },
-  getDiscount() {
-    return discount;
-  },
-  setDiscount(value) {
-    discount = value;
-  },
-  getOrders() {
-    return orders;
-  },
-  addOrder(cost, order) {
-    balance -= cost - cost * discount;
-    orders.push(order);
-  },
-  // Change code above this line
-};
+////
+// Change code above this line
+//};
 
 const customer = {
   username: "Mango",
@@ -35,14 +14,14 @@ const customer = {
     return this.discount;
   },
   setDiscount(value) {
-    discount = value;
+    this.discount = value;
   },
   getOrders() {
     return this.orders;
   },
   addOrder(cost, order) {
-    balance -= cost - cost * discount;
-    orders.push(order);
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
   },
 };
 
